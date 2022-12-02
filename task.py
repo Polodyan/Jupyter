fda22480f1f23d9cb64c5ac3bf7b78bfa2607988
Poina <polinka77789@mail.ru>
2022-12-02 21:17:16 +0300

    новые папки

---
+++ task.py
@@ -0,0 +1,47 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def func(x, a, b, c, d, e):
+    return a*x**4*np.sin(np.cos(x)) + b*x**3 + c*x**2 + d*x + e
+
+koef = [-12, -18, 5, 10, -30]
+x_limit = [-10, 10]
+x = np.arange(x_limit[0], x_limit[1], 0.1)
+color = 'r'
+x_change = []
+func_direct = -1
+
+def change_color():
+    global color
+    if color == 'r':
+        color = 'b'
+    else:
+        color = 'r'
+    return color
+
+for i in range(len(x) - 1):
+    if func_direct == -1:
+        if func(x[i], *koef) < func(x[i+1], *koef):
+            func_direct = 1
+            x_change.append((x[i], func(x[i], *koef)))
+    else:
+        if func(x[i], *koef) > func(x[i+1], *koef):
+            func_direct = -1
+            x_change.append((x[i], func(x[i], *koef)))
+
+#print(len(x_change))
+#print(x_change)
+
+x_cur = np.arange(x_limit[0], x_change[0][0], 0.1)
+plt.plot(x_cur, func(x_cur, *koef), change_color())
+for i in range(len(x_change) - 1):
+    x_cur = np.arange(x_change[i][0], x_change[i+1][0], 0.1)
+    plt.plot(x_cur, func(x_cur, *koef), change_color())
+x_cur = np.arange(x_change[-1][0], x_limit[1], 0.1)
+plt.plot(x_cur, func(x_cur, *koef), change_color())
+
+
+#plt.plot(x, func(x, *koef))
+plt.show()
